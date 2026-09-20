49705f66b37790734f45ad19d768ef3bc600f8c5
Jonathan <jonathan_artback@icloud.com>
2026-09-20 21:24:50 +0200

    Revert "Update trnscrb to v0.61.0"
    This reverts commit 8cce9f823ac8ed9820f221f1f5c2158d97d7309f.

--- Formula/trnscrb.rb
+++ Formula/trnscrb.rb
@@ -1,8 +1,8 @@
 class Trnscrb < Formula
   desc "Offline meeting transcription for macOS that auto-detects your meetings"
   homepage "https://github.com/artback/trnscrb"
-  url "https://github.com/artback/trnscrb/archive/refs/tags/v0.61.0.tar.gz"
-  sha256 "81a0b828f67b0c22b847f8b88a46e2121cc7d25857f680e7dbc2d9f86c07e749"
+  url "https://github.com/artback/trnscrb/archive/refs/tags/v0.60.0.tar.gz"
+  sha256 "627764287c31329ebe6034a77add9920aeaf3e4769e510ca2812899b74ad7d03"
   license "MIT"
   head "https://github.com/artback/trnscrb.git", branch: "main"
 
